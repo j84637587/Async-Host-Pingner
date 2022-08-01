4b31f3082080f9e6933f1b3a3047a1be80512f90
Quareta <j84637587@gmail.com>
2022-08-02 06:12:12 +0800

    feat: Remove unneeded files

--- main.py
+++ main.py
@@ -6,30 +6,10 @@ import asyncio
 import json
 import time
 
-IP_regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 ttl_pattern = re.compile("ttl=(\d+)")
 bytes_pattern = re.compile("(\d+)\sbytes")
+timems_pattern = re.compile("time\=(.*)\sms")
 out = open("out.json", "w+")
-
-# def validIP(ip: str) -> bool:
-#     """
-#     驗證字串是否為ip格式
-
-#     Args:
-#         ip (str): ip字串
-
-#     Returns:
-#         bool: 字串是否為ip格式
-#     """
-#     return re.search(IP_regex, str)
-
-# def validHostname(hostname: str) -> bool:
-#     if len(hostname) > 255:
-#         return False
-#     if hostname[-1] == ".":
-#         hostname = hostname[:-1] # strip exactly one dot from the right, if present
-#     allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
-#     return all(allowed.match(x) for x in hostname.split("."))
 
 def readIPs() -> list:
     """
@@ -44,17 +24,13 @@ def readIPs() -> list:
         for idx, line in enumerate(lines):
             line = line.rstrip()
             ips.append(line)
-            # if True: #validIP(line) or :
-            #     ips.append(line)
-            # else:
-            #     print(f"Line {idx+1} is not a valid ip, skip this time!")
         return ips
 
 
 logs = []
 
 
-def logger(ip: str, rtn: int, ttl: str, bytes: str, stderr: str) -> None:
+def logger(ip: str, rtn: int, ttl: str, bytes: str, timems: str, stderr: str) -> None:
     """
     紀錄事件並輸出
 
@@ -63,12 +39,13 @@ def logger(ip: str, rtn: int, ttl: str, bytes: str, stderr: str) -> None:
         rtn (int): Ping回傳值
         ttl (str): 要記錄的ttl(time to live)
         bytes (str): 要記錄的bytes
+        timems (str): timems
         stderr (str): 錯誤訊息
     """
     now = datetime.now()
     dts = now.strftime("%d/%m/%Y %H:%M:%S")
     if rtn == 0:
-        r = f"[{dts}] IP: {ip:30} is up      TTL: {ttl:4}    Bytes: {bytes:5}"
+        r = f"[{dts}] IP: {ip:30} is up      TTL: {ttl:4}    Bytes: {bytes:5}    Time: {timems:6} ms"
     else:
         stderr = stderr.rstrip("\n")
         r = f"[{dts}] IP: {ip:30} is down    " + (f"Error: {stderr}" if stderr != "" else "")
@@ -87,7 +64,7 @@ async def ping(ip: str) -> None:
         ip (str): 要ping的ip
     """
     proc = await asyncio.create_subprocess_shell(
-        f"ping -c 1 -s 12 {ip}",  # 設置ttl(-t)並不會改變回傳封包的ttl
+        f"ping -c 1 {ip}",  # 設置ttl(-t)並不會改變回傳封包的ttl
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
@@ -98,9 +75,15 @@ async def ping(ip: str) -> None:
     if proc.returncode == 0:
         bytes = bytes_pattern.search(stdout).group(1)
         ttl = ttl_pattern.search(stdout).group(1)
+        
+        a = timems_pattern.search(stdout)
+        try:
+            timems = a.group(1)
+        except AttributeError as error:
+            print("<<<<<<", stdout, ">>>>>>>>")
     else:
-        bytes = ttl = 0
-    logger(ip, proc.returncode, ttl, bytes, stderr)
+        bytes = ttl = timems = 0
+    logger(ip, proc.returncode, ttl, bytes, timems, stderr)
 
 
 async def main(ips: list) -> None:
